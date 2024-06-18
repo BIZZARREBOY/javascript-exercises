7d6b69cc70dfd90eff641c0b9d76cdc21e079f84
bizzarreboy <luigod99@gmail.com>
2024-06-17 22:54:22 -0600

    Solves of excercises from 06 to 12

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,7 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);//(false || true) = true && true
 };
 
+//console.log(leapYears(1900));
 // Do not edit below this line
 module.exports = leapYears;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(nArg) {//n = 6
+    let n;
 
+    if(typeof nArg !== 'number')    n = parseInt(nArg);
+    else    n = nArg;
+
+    if(n < 0)   return "OOPS";
+
+    let first = 0;//fisrt = 0
+    let second = 1;//second = 1
+    let current = 0;
+
+    for (i = 1; i <= n; i++) {//i = 6; 6 <= 6
+        current = first + second;//current = 5 + 3 = 8
+        second = first;//second = 3
+        first = current;//first = 5
+    }
+    return current;
 };
 
 // Do not edit below this line
